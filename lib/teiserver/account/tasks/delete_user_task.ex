385cc39fb33f2ab8c836aec9bcc1c1b0692557ec
jere0500 <jeremias.giesecke@tum.de>
2024-05-20 15:14:36 +0200

    Experimental: uncommented some querry code to make deletion work

--- lib/teiserver/account/tasks/delete_user_task.ex
+++ lib/teiserver/account/tasks/delete_user_task.ex
@@ -11,6 +11,9 @@ defmodule Teiserver.Admin.DeleteUserTask do
     id_list
     |> Enum.each(&Account.decache_user/1)
 
+
+    int_id_list = Enum.map(id_list, fn x -> String.to_integer(x) end)
+
     [
       # Clan memberships
       "DELETE FROM teiserver_clan_memberships WHERE user_id = ANY($1)",
@@ -24,15 +27,15 @@ defmodule Teiserver.Admin.DeleteUserTask do
       "DELETE FROM account_friends WHERE user1_id = ANY($1) OR user2_id = ANY($1)",
 
       # Telemetry
-      "DELETE FROM telemetry_complex_client_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_simple_client_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_complex_match_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_simple_match_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_complex_lobby_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_simple_lobby_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_complex_server_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_simple_server_event_types WHERE user_id = ANY($1)",
-      "DELETE FROM telemetry_user_properties WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_complex_client_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_simple_client_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_complex_match_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_simple_match_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_complex_lobby_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_simple_lobby_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_complex_server_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_simple_server_event_types WHERE user_id = ANY($1)",
+      # "DELETE FROM telemetry_user_properties WHERE user_id = ANY($1)",
 
       # User table extensions/stats
       "DELETE FROM teiserver_account_user_stats WHERE user_id = ANY($1)",
@@ -47,7 +50,7 @@ defmodule Teiserver.Admin.DeleteUserTask do
       # Chat
       "DELETE FROM teiserver_lobby_messages WHERE user_id = ANY($1)",
       "DELETE FROM teiserver_room_messages WHERE user_id = ANY($1)",
-      "DELETE FROM direct_messages WHERE from_user_id = ANY($1) OR to_user_id = ANY($1)",
+      # "DELETE FROM direct_messages WHERE from_user_id = ANY($1) OR to_user_id = ANY($1)",
 
       # Match stuff
       "DELETE FROM teiserver_battle_match_memberships WHERE user_id = ANY($1)",
@@ -55,16 +58,16 @@ defmodule Teiserver.Admin.DeleteUserTask do
       "DELETE FROM teiserver_game_rating_logs WHERE user_id = ANY($1)",
 
       # Moderation
-      "DELETE FROM moderation_reports WHERE reporter_id = ANY($1) OR target_id = ANY($1) OR responder_id = ANY($1)",
+      # "DELETE FROM moderation_reports WHERE reporter_id = ANY($1) OR target_id = ANY($1) OR responder_id = ANY($1)",
       "DELETE FROM moderation_actions WHERE target_id = ANY($1)"
     ]
     |> Enum.each(fn query ->
-      Ecto.Adapters.SQL.query!(Repo, query, [id_list])
+      Ecto.Adapters.SQL.query!(Repo, query, [int_id_list])
     end)
 
     # And now the users
     query = "DELETE FROM account_users WHERE id = ANY($1)"
-    Ecto.Adapters.SQL.query!(Repo, query, [id_list])
+    Ecto.Adapters.SQL.query!(Repo, query, [int_id_list])
 
     # Delete our cache of them
     id_list
